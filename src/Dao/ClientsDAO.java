fde2e33f3706f670a1faca52964bc7a87162b4d9
Emily Renard <emily.renard@edu.esiee.fr>
2021-06-08 14:54:25 +0200

    DAO pour Clients avec méthodes find pour rechecher une valeur spécifique.

--- src/Dao/ClientsDAO.java
+++ src/Dao/ClientsDAO.java
@@ -1,11 +1,80 @@
 package Dao;
 
+import DataAccess.JdbcConnectDB;
 import Domain.Clients;
+import lombok.SneakyThrows;
 import org.apache.log4j.Logger;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class ClientsDAO {
+
+public class ClientsDAO implements Dao<Clients>{
     private static final Logger logger = Logger.getLogger(ClientsDAO.class);
-    private static HashMap<Integer, Clients> cache= new HashMap<Integer, Clients>()
+    private static HashMap<Integer, Clients> cache= new HashMap<Integer, Clients>();
+
+    /**
+     * Returns Client that matches the id param.
+     * @param id
+     * @return
+     */
+    public Clients findClientsById(int id){
+        if(cache.containsKey(id)){
+            return cache.get(id);
+        }
+        List<Clients> clients = find("WHERE id_clients = "+id);
+        return clients.get(0);
+    }
+
+    @SneakyThrows
+    public List<Clients> find(String query) /*throws SQLException*/ {
+        List<Clients> clientsList = new ArrayList<Clients>();
+        Statement statement = JdbcConnectDB.getConnection().createStatement();
+
+        String sqlQuery = "SELECT * FROM Clients "+ query;
+        try {
+            logger.debug("executing query : "+ sqlQuery);
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            while( resultSet.next()){
+                Clients clients = resultSetToClients(resultSet);
+                clientsList.add(clients);
+            }
+        } catch (SQLException sqlException){
+            logger.error("error executing: "+sqlQuery, sqlException);
+        } finally {
+            if(statement != null) try {
+                statement.close();
+            }catch (SQLException e){}
+
+        }
+        return clientsList;
+    }
+
+    private Clients resultSetToClients(ResultSet resultSet) throws SQLException{
+        Clients clients = null;
+
+        Integer id = resultSet.getInt("id_clients");
+        if(cache.containsKey(id)) clients =cache.get(id);
+        else clients = new Clients();
+
+        clients.setIdClient(id);
+        clients.setAdresse(resultSet.getString("adresse"));
+        clients.setDepenses(resultSet.getFloat("depenses"));
+        clients.setNom(resultSet.getString("nom"));
+        clients.setPizzaAchete(resultSet.getInt("pizza_achete"));
+        clients.setPrenom(resultSet.getString("prenom"));
+        clients.setSolde(resultSet.getFloat("solde"));
+        clients.setTelephone(resultSet.getString("telephone"));
+
+        if(! cache.containsKey(id)) cache.put(id, clients);
+
+        logger.info("get clients for order "+clients.getNom());
+        return clients;
+    }
+
+
 }
---
+++ src/DataAccess/JdbcConnectDB.java
@@ -0,0 +1,63 @@
+package DataAccess;
+
+import Dao.ClientsDAO;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class JdbcConnectDB {
+    private static String url = "jbdc:mysql:/RAPIZZ";
+    private static String uname = "Admin";
+    private static String password = "RichardEtEmilyMeritentUn20/20";
+    private static String driver = "com.mysql.jbdc.Driver";
+    private static Connection connection = null;
+
+    private static Logger logger = Logger.getLogger(JdbcConnectDB.class);
+    private JdbcConnectDB(){}
+
+    public static Connection getConnection() throws SQLException {
+        if(connection == null){
+            connection = makeConnection();
+        }
+        return connection;
+    }
+
+    private static Connection makeConnection() throws SQLException {
+        try {
+            Class c = Class.forName(driver);
+            // load the database driver class.
+            connection = DriverManager.getConnection(url, uname, password);
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+            logger.error("connection failed ", e);
+            throw new SQLException(e);
+        }
+        return connection;
+    }
+    /* public static void main(String[] args) throws SQLException {
+        String url = "jbdc:mysql:/RAPIZZ";
+        String uname = "Admin";
+        String password = "RichardEtEmilyMeritentUn20/20";
+
+        try {
+            Class c = Class.forName("com.mysql.jbdc.Driver");
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try{
+            Connection con = DriverManager.getConnection(url,uname,password);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }*/
+
+}
