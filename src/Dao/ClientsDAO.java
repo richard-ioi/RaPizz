90f402163a7a54a12d111625913215f2e9f68e4d
Richard FOUQUOIRE <richard.fouquoire@edu.esiee.fr>
2021-06-08 12:06:47 +0200

    Ajout point virgule

--- src/Dao/ClientsDAO.java
+++ src/Dao/ClientsDAO.java
@@ -7,5 +7,5 @@ import java.util.HashMap;
 
 public class ClientsDAO {
     private static final Logger logger = Logger.getLogger(ClientsDAO.class);
-    private static HashMap<Integer, Clients> cache= new HashMap<Integer, Clients>()
+    private static HashMap<Integer, Clients> cache= new HashMap<Integer, Clients>();
 }
