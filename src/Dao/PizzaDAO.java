63181b549415147038b3b011eef56719a6f1ed02
Richard FOUQUOIRE <richard.fouquoire@edu.esiee.fr>
2021-06-09 19:54:11 +0200

    Ajout des derniers dao

--- src/Dao/PizzaDAO.java
+++ src/Dao/PizzaDAO.java
@@ -1,4 +1,71 @@
 package Dao;
 
+import DataAccess.JdbcConnectDB;
+import Domain.Pizza;
+import lombok.SneakyThrows;
+import org.apache.log4j.Logger;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class PizzaDAO {
+    private static final Logger logger = Logger.getLogger(PizzaDAO.class);
+    private static HashMap<Integer, Pizza> cache= new HashMap<Integer, Pizza>();
+
+    /**
+     * Returns Pizza that matches the id param.
+     * @param id
+     * @return
+     */
+    public Pizza findPizzaById(int id){
+        if(cache.containsKey(id)){
+            return cache.get(id);
+        }
+        List<Pizza> pizza = find("WHERE id_pizza = "+id);
+        return pizza.get(0);
+    }
+
+    @SneakyThrows
+    public List<Pizza> find(String query) /*throws SQLException*/ {
+        List<Pizza> pizzaList = new ArrayList<>();
+        Statement statement = JdbcConnectDB.getConnection().createStatement();
+
+        String sqlQuery = "SELECT * FROM Pizza "+ query;
+        try {
+            logger.debug("executing query : "+ sqlQuery);
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            while( resultSet.next()){
+                Pizza pizza = resultSetToPizza(resultSet);
+                pizzaList.add(pizza);
+            }
+        } catch (SQLException sqlException){
+            logger.error("error executing: "+sqlQuery, sqlException);
+        } finally {
+            if(statement != null) try {
+                statement.close();
+            }catch (SQLException e){}
+
+        }
+        return pizzaList;
+    }
+    private Pizza resultSetToPizza(ResultSet resultSet) throws SQLException{
+        Pizza pizza = null;
+
+        Integer id = resultSet.getInt("id_pizza");
+        if(cache.containsKey(id)) pizza =cache.get(id);
+        else pizza = new Pizza();
+
+        pizza.setIdPizza(id);
+        pizza.setNom(resultSet.getString("nom"));
+        pizza.setPrix(resultSet.getInt("prix"));
+
+        if(! cache.containsKey(id)) cache.put(id, pizza);
+
+        logger.info("get pizza for order "+pizza.getNom());
+        return pizza;
+    }
 }
--- src/Dao/VehiculeDAO.java
+++ src/Dao/VehiculeDAO.java
@@ -1,4 +1,71 @@
 package Dao;
 
+import DataAccess.JdbcConnectDB;
+import Domain.Vehicule;
+import lombok.SneakyThrows;
+import org.apache.log4j.Logger;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class VehiculeDAO {
+    private static final Logger logger = Logger.getLogger(VehiculeDAO.class);
+    private static HashMap<Integer, Vehicule> cache= new HashMap<Integer, Vehicule>();
+
+    /**
+     * Returns Vehicule that matches the id param.
+     * @param id
+     * @return
+     */
+    public Vehicule findVehiculeById(int id){
+        if(cache.containsKey(id)){
+            return cache.get(id);
+        }
+        List<Vehicule> vehicule = find("WHERE id_vehicule = "+id);
+        return vehicule.get(0);
+    }
+
+    @SneakyThrows
+    public List<Vehicule> find(String query) /*throws SQLException*/ {
+        List<Vehicule> vehiculeList = new ArrayList<>();
+        Statement statement = JdbcConnectDB.getConnection().createStatement();
+
+        String sqlQuery = "SELECT * FROM Vehicule "+ query;
+        try {
+            logger.debug("executing query : "+ sqlQuery);
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            while( resultSet.next()){
+                Vehicule vehicule = resultSetToVehicule(resultSet);
+                vehiculeList.add(vehicule);
+            }
+        } catch (SQLException sqlException){
+            logger.error("error executing: "+sqlQuery, sqlException);
+        } finally {
+            if(statement != null) try {
+                statement.close();
+            }catch (SQLException e){}
+
+        }
+        return vehiculeList;
+    }
+    private Vehicule resultSetToVehicule(ResultSet resultSet) throws SQLException{
+        Vehicule vehicule = null;
+
+        Integer id = resultSet.getInt("id_vehicule");
+        if(cache.containsKey(id)) vehicule =cache.get(id);
+        else vehicule = new Vehicule();
+
+        vehicule.setIdVehicule(id);
+        vehicule.setImmatriculation(resultSet.getString("immatriculation"));
+        vehicule.setType(resultSet.getString("type"));
+
+        if(! cache.containsKey(id)) cache.put(id, vehicule);
+
+        logger.info("get vehicule for order "+vehicule.getIdVehicule());
+        return vehicule;
+    }
 }
