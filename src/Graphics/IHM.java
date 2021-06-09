1145528271919c5feb3183d46fec64f8dfdd849c
Richard FOUQUOIRE <richard.fouquoire@edu.esiee.fr>
2021-06-09 16:21:34 +0200

    Mise en place du squelette IHM

--- src/Graphics/IHM.java
+++ src/Graphics/IHM.java
@@ -2,25 +2,88 @@ package Graphics;
 
 import java.awt.*;
 import java.awt.event.*;
-public class IHM extends Panel
+import javax.swing.*;
+
+public class IHM extends JFrame
         implements ActionListener {
 
-    Button boutonOngletMenu;
-    Button boutonOngletCommandes;
-    Button boutonOngletDonneesBrutes;
-    Panel onglets;
-    Panel menu;
-    Panel commandes;
-    Panel donneesBrutes;
+    JButton boutonOngletMenu;
+    JButton boutonOngletCommandes;
+    JButton boutonOngletDonneesBrutes;
+    JPanel onglets;
+    JScrollPane menu;
+    ScrollPane commandes;
+    ScrollPane donneesBrutes;
+
     public IHM() {
-        onglets = new Panel();
-        menu = new Panel();
+        onglets = new JPanel();
         onglets.setLayout(new GridLayout(1,3));
-        boutonOngletMenu = new Button("Données Brutes");
-        MeDonnées brutes}
+        boutonOngletMenu = new JButton("Menu");
+        boutonOngletCommandes=new JButton("Commandes");
+        boutonOngletDonneesBrutes=new JButton("Données brutes");
+        onglets.add(boutonOngletMenu);
+        onglets.add(boutonOngletCommandes);
+        onglets.add(boutonOngletDonneesBrutes);
+        boutonOngletMenu.addActionListener(this);
+        boutonOngletCommandes.addActionListener(this);
+        boutonOngletDonneesBrutes.addActionListener(this);
+        getMenuScrollPane();
+        getCommandesScrollPane();
+        getDonneesBrutesScrollPane();
+        setLayout(new BorderLayout());
+        add(onglets, BorderLayout.NORTH);
+        add(menu, BorderLayout.CENTER);
+    }
 
-    @OvCommandesde
-    public vCommandesactionPerformed(ActionEvent e) {
+    public void getMenuScrollPane(){
+        JPanel test=new JPanel();
+        test.setLayout(new GridLayout(20,1));
+        //menu.setLayout(new BoxLayout(menu, BoxLayout.PAGE_AXIS));
+        test.add(new Label("Bienvenue sur l'onglet menu"));
+        test.add(new Label("TEST1"));
+        test.add(new Label("TEST2"));
+        test.add(new Label("TEST3"));
+        menu=new JScrollPane(test);
+    }
 
+    public void getCommandesScrollPane(){
+        commandes = new ScrollPane();
+        commandes.add(new Label("Bienvenue sur l'onglet commandes"));
+    }
+
+    public void getDonneesBrutesScrollPane(){
+        donneesBrutes = new ScrollPane();
+        donneesBrutes.add(new Label("Bienvenue sur l'onglet données brutes"));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        Object source = evt.getSource();
+
+        getContentPane().removeAll();
+        getContentPane().add(onglets, BorderLayout.NORTH);
+        if (source == boutonOngletMenu){
+            getContentPane().add(menu);
+        }
+        if (source == boutonOngletCommandes){
+            getContentPane().add(commandes);
+        }
+        if (source == boutonOngletDonneesBrutes){
+            getContentPane().add(donneesBrutes);
+        }
+        repaint();
+        printAll(getGraphics());//Extort print all content
+    }
+
+    public static void main(String[] arg)
+    {
+        JFrame f = new IHM();
+        f.setSize(800, 400);
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(0);
+            }} );
+        f.setVisible(true);
     }
 }
